0f09e1ee598a2e5b02f28ae692afccc90130ae62
aduBoahene <boahenepatrick2@gmail.com>
2024-09-16 22:30:46 +0000

    added CI pipeline

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
+
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const { sequelize } = require('./models'); // Import Sequelize instance
 const { Student } = require('./models');
 const logger = require('morgan');
+const {errorHandler, AppError} = require('./errorHandler');
 
 
 app.use(express.json());
 app.use(logger('dev'))
+
 
 app.get("/", (req,res)=>{
     res.json({msg:"Welcome to SRE bootcamp"})
 })
 
 
-app.post("/api/v1/student", async (req, res) => {
+app.post("/api/v1/student", async (req, res, next) => {
     try {
         const { firstName, lastName, email, studentId, level, program } = req.body;
         const newStudent = await Student.create({ firstName, lastName, email, studentId, level, program })
@@ -29,23 +33,33 @@ app.post("/api/v1/student", async (req, res) => {
                 data: newStudent
             })
     } catch (error) {
-        console.log(error)
+        next(error)
     }
 
 })
 
-app.get("/api/v1/student", async (req, res) => {
+app.get('/test-error', (req, res, next) => {
+    // Simulate a synchronous error
+    throw new AppError('Test error', 500);
+
+  });
+
+
+
+
+app.get("/api/v1/student", async (req, res, next) => {
     try {
+        // const students = await Student.findAll({})
         const students = await Student.findAll({})
         res.json({ data: students })
     } catch (error) {
-        console.log(error)
+        next(error)
     }
 
 })
 
 // Fetch endpoint to fetch a student by ID
-app.get('/api/v1/student/:id', async (req, res) => {
+app.get('/api/v1/student/:id', async (req, res, next) => {
     const { id } = req.params;
 
     try {
@@ -58,11 +72,12 @@ app.get('/api/v1/student/:id', async (req, res) => {
         res.json({ msg: 'Student found successfully', data: student });
     } catch (error) {
         res.status(500).json({ error: 'An error occurred while deleting the student' });
+        next(error)
     }
 });
 
 // PUT endpoint to update a student by ID
-app.put('/api/v1/student/:id', async (req, res) => {
+app.put('/api/v1/student/:id', async (req, res, next) => {
     const { id } = req.params;
     const { firstName, lastName, email, studentId, level, program } = req.body; // Assuming these are the fields you want to update
 
@@ -84,11 +99,12 @@ app.put('/api/v1/student/:id', async (req, res) => {
         res.json(student);
     } catch (error) {
         res.status(500).json({ error: 'An error occurred while updating the student' });
+        next(error)
     }
 });
 
 // Delete endpoint to Delete a student by ID
-app.delete('/api/v1/student/:id', async (req, res) => {
+app.delete('/api/v1/student/:id', async (req, res, next) => {
     const { id } = req.params;
 
     try {
@@ -102,15 +118,24 @@ app.delete('/api/v1/student/:id', async (req, res) => {
         res.json({ message: 'Student deleted successfully' });
     } catch (error) {
         res.status(500).json({ error: 'An error occurred while deleting the student' });
+        next(error)
     }
 });
 
 
 
 // Healthcheck endpoint
-app.get('/healthcheck', (req, res) => {
+app.get('/healthcheck', (req, res, next) => {
     res.status(200).json({ status: 'ok' });
 });
+
+// For unhandled routes
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Error handling middleware should be last
+app.use(errorHandler);
 
 
 
